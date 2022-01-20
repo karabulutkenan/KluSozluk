860de6ba10c4a8f421db77887741bf8c7ca91689
karabulutkenan <karabulutkenan@gmail.com>
2022-01-20 23:58:36 +0300

    Entity Layer Completed

---
+++ EntityLayer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EntityLayer.MvcApplication" Language="C#" %>
---
+++ KluSozluk/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace KluSozluk
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
